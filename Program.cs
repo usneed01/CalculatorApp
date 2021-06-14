9b455cf4bf4900743e1b6923e33d3c46f825e4d0
Ulyssa Sneed <ulyssasneed@Ulyssas-MBP.attlocal.net>
2021-06-13 23:14:00 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------------------------------------------------\n" +
+                "----------------------Welcome To Coolbeans Console Calculator---------------\n" +
+                "----------------------------------------------------------------------------");
+
+            Console.WriteLine("Enter Your First Number: ");
+            double fnum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Second Number");
+            double snum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select An Operation: (-,+, *, /)");
+        }
+    }
+}
