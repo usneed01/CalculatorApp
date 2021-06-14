ed3979d89c2982745f763be46544b3d7eaa943c2
Ulyssa Sneed <ulyssasneed@Ulyssas-MBP.attlocal.net>
2021-06-14 03:20:38 -0400

    adjusting results

--- calculatorMachine.cs
+++ calculatorMachine.cs
@@ -7,53 +7,58 @@ using System.Threading.Tasks;
 
 namespace CalculatorApp
     {
-        class calculatorMachine
+    class calculatorMachine
 
+    {
+        private double firstNumber;
+        private double secondNumber;
+        private double results;
+        private char operation;
+
+        public calculatorMachine(double first, double second, char opp)
         {
-            private double firstNumber;
-            private double secondNumber;
-            private double results;
-            private char operation;
-
-            public calculatorMachine(double first, double second, char opp)
-            {
-                firstNumber     = first;
-                secondNumber    = second;
-                operation       = opp;
-                Switching();
-            }
-            private void Switching()
+            firstNumber = first;
+            secondNumber = second;
+            operation = opp;
+            Switching();
+        }
+        private void Switching()
         {
             switch (operation)
             {
                 case '+':
                     results = firstNumber + secondNumber;
                     Console.WriteLine("The sum of {0} and {1} is {2}", firstNumber, secondNumber, results);
+                    startover();
                     break;
 
                 case '-':
                     results = firstNumber - secondNumber;
                     Console.WriteLine("The difference of {0} and {1} is {2}", firstNumber, secondNumber, results);
                     break;
+                    startover();
 
                 case '*':
                     results = firstNumber * secondNumber;
                     Console.WriteLine("The product of {0} and {1} is {2}", firstNumber, secondNumber, results);
                     break;
+                    startover();
 
                 case '/':
                     results = firstNumber / secondNumber;
                     Console.WriteLine("The division of {0} and {1} is {2}", firstNumber, secondNumber, results);
                     break;
+                    startover();
 
 
                 default:
                     Console.WriteLine("Impossible Action! Please Try Again!");
+                    End();
                     break;
 
             }
         }
-            private void startover()
+        private void startover()
         {
             Console.WriteLine("Want To Do Something Else??\nIf \'yes\'the press and enter (Y). \n" +
                 "Enter any key");
@@ -61,11 +66,13 @@ namespace CalculatorApp
             bool mybool = (YesorNo == "y" || YesorNo == "Y");
             if (mybool == true)
             {
+                repeat();
             }
             else
-            { 
+            {
+                End();
             }
-       
+
         }
 
         private void repeat()
@@ -73,9 +80,23 @@ namespace CalculatorApp
             firstNumber = results;
             Console.WriteLine("Enter Your Second Number:");
             secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select An Operation: (- , + , * , /)");
+            operation = char.Parse(Console.ReadLine());
+            Switching();
+
         }
 
+        private void End()
+        {
 
+            Console.WriteLine("Final Result Expected is {0}", results);
+            Console.WriteLine("Press Any Key to ESC the calculator");
+            Console.ReadLine();
         }
 
     }
+           
+        }
+
+   
